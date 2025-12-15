908acea34a7b2a8b9f8c6761083bd53ecb7efe88
Taka <103984069+Taka0007@users.noreply.github.com>
2025-12-15 14:40:06 +0900

    Create lstm.py

---
+++ pgportfolio/model/lstm.py
@@ -0,0 +1,15 @@
+# /pgportfolio/model/lstm.py
+from .base import BaseModel
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EIILSTM(BaseModel):
+    def __init__(self, assets, hidden):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=assets, hidden_size=hidden, batch_first=True)
+        self.fc = nn.Linear(hidden, assets)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = out[:, -1]
+        return torch.softmax(self.fc(out), dim=1)
