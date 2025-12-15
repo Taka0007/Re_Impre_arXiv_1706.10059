c1fd65184fe9ce88a0a4c1e51522faf69b33248d
Taka <103984069+Taka0007@users.noreply.github.com>
2025-12-15 14:41:54 +0900

    Create base.py

---
+++ pgportfolio/model/base.py
@@ -0,0 +1,9 @@
+# /pgportfolio/model/base.py
+import torch.nn as nn
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        raise NotImplementedError
