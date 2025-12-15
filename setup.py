0556649a38748f12bab96863757278a5ea78b9f9
Taka <103984069+Taka0007@users.noreply.github.com>
2025-12-15 14:25:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
+# Re_Impre_arXiv_1706.10059/setup.py
+from setuptools import setup, find_packages
 
+setup(
+    name="pgportfolio_pytorch",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "yfinance",
+        "tqdm",
+        "scipy",
+    ],
+    description="A PyTorch implementation of PGPortfolio (Policy Gradient portfolio)",
+)
