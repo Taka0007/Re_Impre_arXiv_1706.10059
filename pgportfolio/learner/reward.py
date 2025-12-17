78fccb9264e4c8465ed2403fa1bf0bed3c37ff8f
Taka <103984069+Taka0007@users.noreply.github.com>
2025-12-17 13:54:09 +0900

    Update reward.py

--- pgportfolio/learner/reward.py
+++ pgportfolio/learner/reward.py
@@ -3,5 +3,6 @@ import torch
 
 def immediate_reward(w, price_rel, prev_w, commission):
     gross = (w * price_rel).sum(dim=1)
+    return torch.log(gross + 1e-8)
     #cost = commission * (w - prev_w).abs().sum(dim=1)
-    return torch.log(gross - cost + 1e-8)
+    #return torch.log(gross - cost + 1e-8)
